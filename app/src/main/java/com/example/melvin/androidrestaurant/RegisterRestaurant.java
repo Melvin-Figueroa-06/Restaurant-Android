a86fcb8cc893a05f3798dd3a8cbd9a4dd967ce86
melvin figueroa <melproducc@gmail.com>
2018-12-07 03:53:47 -0400

    first commit

--- app/src/main/java/com/example/melvin/androidrestaurant/RegisterRestaurant.java
+++ app/src/main/java/com/example/melvin/androidrestaurant/RegisterRestaurant.java
@@ -1,11 +1,15 @@
 package com.example.melvin.androidrestaurant;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,11 +17,19 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class RegisterRestaurant extends AppCompatActivity implements OnMapReadyCallback{
     private MapView map;
     private GoogleMap mMap;
+    private  Geocoder geocoder;
+    private TextView street;
+    private Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,18 +39,61 @@ public class RegisterRestaurant extends AppCompatActivity implements OnMapReadyC
         map.onResume();
         MapsInitializer.initialize(this);
         map.getMapAsync(this);
+        geocoder = new Geocoder(getBaseContext(), Locale.getDefault());
+        street = findViewById(R.id.street);
+        next = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+          //validar
+                senData();
+            }
+        });
 
     }
+    public  void senData(){
 
-
+    }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //19.5806069,-65.7632351
+        LatLng sydney = new LatLng(-19.5806069, -65.7632351);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Lugar").zIndex(17).draggable(true));
+        mMap.setMinZoomPreference(16);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                String street_string = getStreet(marker.getPosition().latitude, marker.getPosition().longitude);
+                street.setText(street_string);
+            }
+        });
+    }
+    public String getStreet (Double  lat, Double lon){
+        List<Address> address;
+        String result ="";
+        try {
+            address = geocoder.getFromLocation(lat, lon, 3);
+            result += address.get(0).getThoroughfare();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
 }
---
+++ app/src/main/java/com/example/melvin/androidrestaurant/utils/Data.java
@@ -0,0 +1,7 @@
+package com.example.melvin.androidrestaurant.utils;
+
+public class Data {
+    public static String TOKEN ="";
+    public static String HOST = "http://192168.1.3";
+    public static String REGISTER_RESTAURANT = "/restaurant";
+}
